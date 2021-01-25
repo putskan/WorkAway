115076bbb01c65965b44958e1113a4f1666c32b8
putskan <mule9003@gmail.com>
2021-01-25 21:17:09 +0200

    Add Node.js backend & mongoDB

---
+++ be/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const authRoute = require('./routes/auth');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+async function dbConnect() {
+    try {
+        await mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true });
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+
+dotenv.config()
+dbConnect();
+// add post recieval functionallity
+app.use(express.json());
+// routes
+app.use('/api/user', authRoute);
+
+app.listen(9000, () => console.log('UP!'));
--- fe/src/components/Navbars/Navbar.js
+++ fe/src/components/Navbars/Navbar.js
@@ -3,7 +3,7 @@ import classNames from "classnames";
 import PropTypes from "prop-types";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
+import { AppBar } from '@material-ui/core';
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Hidden from "@material-ui/core/Hidden";
