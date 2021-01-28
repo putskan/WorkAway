e68a8af2f3a8e91d8cbfc13964127f08f345aeb5
putskan <mule9003@gmail.com>
2021-01-29 01:36:52 +0200

    Test Deployment

---
+++ client/build/precache-manifest.a0f71a29aea6dd82110d98b800b72ba0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ca0b6747e765479d9d834fa275f6a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa65d0ed06e2e094b4df",
+    "url": "./static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "198b285909b92cd817e2",
+    "url": "./static/css/main.e7a13ebf.chunk.css"
+  },
+  {
+    "revision": "fa65d0ed06e2e094b4df",
+    "url": "./static/js/2.29400d07.chunk.js"
+  },
+  {
+    "revision": "198b285909b92cd817e2",
+    "url": "./static/js/main.b2c2471a.chunk.js"
+  },
+  {
+    "revision": "c3775b24d7869a22637f",
+    "url": "./static/js/runtime~main.3451b984.js"
+  },
+  {
+    "revision": "90bb3c7ce3c6a03e40fa3b3b672a0cd9",
+    "url": "./static/media/cover.90bb3c7c.jpeg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "./static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "./static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "./static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "./static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "./static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
